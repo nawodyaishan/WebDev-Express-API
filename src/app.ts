b42a005d100d31d0ce2acf99b66fa13f293e6cd8
nawodyaishan <nawodyain@gmail.com>
2022-11-10 00:14:30 +0530

    Add basic post with json middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import {Request, Response, NextFunction} from "express";
 import express from "express";
 
-
 const app = express();
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req: Request<string>, res: Response<string>, next: NextFunction) => {
     return res.send("Hello World!!");
 });
+
+app.post(`/api/data`, (req: Request, res: Response) => {
+    console.log(req.body);
+
+    return res.sendStatus(200);
+})
 
 app.listen(3000, () => {
     console.log("Application Listening at local port http://localhost:3000")
