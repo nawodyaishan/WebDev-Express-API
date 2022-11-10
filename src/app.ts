7ed6dd10f84b004d92db9909485568cedbec6304
nawodyaishan <nawodyain@gmail.com>
2022-11-10 14:46:32 +0530

    Add get all

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import {Request, Response, NextFunction} from "express";
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 
 const app = express();
 
@@ -7,7 +6,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get("/", (req: Request<string>, res: Response<string>, next: NextFunction) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
     return res.send("Hello World!!");
 });
 
@@ -17,6 +16,11 @@ app.post(`/api/data`, (req: Request, res: Response) => {
     return res.sendStatus(200);
 })
 
+app.all(`/api/all`, (req: Request, res: Response) => {
+    return res.sendStatus(200);
+})
+
 app.listen(3000, () => {
     console.log("Application Listening at local port http://localhost:3000")
 })
+
